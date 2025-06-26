3e45a6f743dbc75e33767d6dc8f57f63bd81624f
AdityaGaikwad6430 <adityagaikwad6430@gmail.com>
2025-06-26 15:19:15 +0530

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -53,3 +53,18 @@ EOF
 
 sudo systemctl restart docker
 sudo systemctl enable docker
+
+# K8s repo key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | \
+sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# Add repo
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | \
+sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install K8s components
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
